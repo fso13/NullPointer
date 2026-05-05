209801dfeb290d4ee6a6b5f9da4673d857010edb
Rudenko Dmitry <dimarudenk@gmail.com>
2026-05-05 18:10:07 +0300

    счяс

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
